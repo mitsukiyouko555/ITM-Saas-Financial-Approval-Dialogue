69c3d177b3cdffe8ee99f8065373ca3704fa491b
mitsukiyouko555 <25854884+mitsukiyouko555@users.noreply.github.com>
2024-03-02 23:22:07 -0800

    figured some things outgit status yaygit status

--- src/components/dropdowns.tsx
+++ src/components/dropdowns.tsx
@@ -1,34 +1,21 @@
 //parameter 1/[0] is the id
 //parameter 2/[1] is the class(es)
 //parameter3/[2] is the optionName
-
-//how will the fieldname be defined? should the same fieldname be defined for each
+//Parameter 4/[3] is the FIELD Name (goes in the label ONLY)
 
 function Dropdowns(...params:any[]){
-    console.log(params[0][0])
-    console.log(params[1])
-    console.log(params[2])
+    // NOTE: the ... makes it so that you can put any number of params!! doesnt have to be the same for each component which is super awesome!!
+
 
     return(
-        <>
-            <div className={params[1]}>
-            {/* dropdown select and options go here*/}
- 
+        <div className="dropdowns">
 
-                <label>{params[0][3]}: </label>
+            <label>{params[0][3]}: </label>
+            <select name={params[0][3]}>
+                {params.map((i) => <option key={i[0]} className={i[1]} id={i[0]}> {i[2]} </option>)}
+            </select>
 
-                <select name={params[0][3]} id={params[0][0]} className={params[0][1]}>
-                    <option>{params[0][2]}</option>
-                    <option>{params[1][2]}</option>
-                    <option>{params[2][2]}</option>    
-                    <option>{params[3][2]}</option>  
-                    <option>{params[4][2]}</option>  
-
-                </select>
-
-            </div>
-
-        </>
+        </div>
     )
 
 }
--- src/components/forms.tsx
+++ src/components/forms.tsx
@@ -1,22 +1,39 @@
-// import { useState } from "react";
 import TheFields from "./fields"
-import { visualCapture } from "./handleFunctions/handleVC"
 import Dropdowns from "./dropdowns"
 
-
+//NOTE: use this for itm and endpoint dlp normal, additional, and total users BUT for Enterprise DLP and DPP transform, if the logic gets TOO complicated, you may need to make a special form just for them...(?) But try it here first. most likely you can get it to work with the switch case statements in your handle functions
 function Forms() {
-  // const [count, setCount] = useState(0)
 
   return (
     <>
+
+        {Dropdowns([["new"], ["dealDropdown"], ["New"], ["Deal Type"]], 
+        [["renewal"], ["dealDropdown"], ["Renewal"], ["Deal Type"]], 
+        [["addon"], ["dealDropdown"], ["Add-On"], ["Deal Type"]])}
+
         {TheFields("itmUsers", "formItem", "ITM Saas Users")}
-        {Dropdowns([["itm30days"], ["itmDropdown, itmRetention"], ["30 Days"], ["ITM Saas Retention"]],
+        <p className="wip">Additional ITM Saas Users</p>
+        <p className="wip">Total ITM Saas Users</p>
+        {Dropdowns([["itmDefault"], ["itmDropdown, itmRetention"], ["Select a Retention"], ["ITM Saas Retention"]],
+        [["itm30days"], ["itmDropdown, itmRetention"], ["30 Days"], ["ITM Saas Retention"]],
         [["itm90days"], ["itmDropdown, itmRetention"], ["90 days"] , ["ITM Saas Retention"]],
         [["itm120days"], ["itmDropdown, itmRetention"], ["120 days"] , ["ITM Saas Retention"]],
         [["itm180days"], ["itmDropdown, itmRetention"], ["180 days"] , ["ITM Saas Retention"]],
         [["itm366days"], ["itmDropdown, itmRetention"], ["366 days"] , ["ITM Saas Retention"]],)}
+        <p className="wip">ITM Visual Capture</p>
+        <p className="wip">Additional Visual Capture</p>
+        <p className="wip">Total Visual Capture</p>
 
-    
+        <p className="wip">Endpoint DLP Users</p>
+        <p className="wip">Additional Endpoint DLP Users</p>
+        <p className="wip">Total Endpoint DLP Users</p>
+        
+        {Dropdowns([["endpointDefault"], ["endpointDropdown, endpointRetention"], ["Select a Retention"], ["Endpoint DLP Retention"]],
+        [["endpoint30days"], ["endpointDropdown, endpointRetention"], ["30 Days"], ["Endpoint DLP Retention"]],
+        [["endpoint90days"], ["endpointDropdown, endpointRetention"], ["90 days"] , ["Endpoint DLP Retention"]],
+        [["endpoint120days"], ["endpointDropdown, endpointRetention"], ["120 days"] , ["Endpoint DLP Retention"]],
+        [["endpoint366days"], ["endpointDropdown, endpointRetention"], ["366 days"] , ["Endpoint DLP Retention"]],)}
+
     </>
   )
 }
